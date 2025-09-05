3447b14fb9e359100d1037f1012d8d22c8c047d4
Santiago SILVA <16252054+sssilvar@users.noreply.github.com>
2025-09-05 22:04:11 +0200

    feat: build date in version

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 {
     if (argc > 1 && std::string(argv[1]) == "--version")
     {
-        std::cout << "vv version " << VV_VERSION << std::endl;
+        std::cout << "vv version " << VV_VERSION << " (built " << VV_BUILD_DATE << ")" << std::endl;
         return 0;
     }
     if (argc < 2)
