9768df7b6673b63948556af36da1ad29469c8523
Santiago SILVA <16252054+sssilvar@users.noreply.github.com>
2025-09-05 22:01:20 +0200

    chore: improve install script and fix paths

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 set -e
 
+run() {
+  CYAN='\033[0;36m'
+  YELLOW='\033[1;33m'
+  NC='\033[0m'
+  printf "${CYAN}>>${YELLOW} %s${NC}\n" "$*"
+  eval "$@"
+}
+
+export VCPKG_ROOT=$(dirname "$(which vcpkg)")
+INSTALL_DIR=${INSTALL_DIR:-~/.local/bin}
+run "mkdir -p $INSTALL_DIR"
+
 # Detect platform and set preset
 if [ "$(uname)" = "Darwin" ]; then
   PRESET=macos-release
@@ -11,9 +23,9 @@ else
   exit 1
 fi
 
+run "cmake --preset=$PRESET"
+run "cmake --build --preset=$PRESET"
+run "cp ./build/vv $INSTALL_DIR"
 
-cmake --preset=$PRESET
-cmake --build --preset=$PRESET
-cp ./out/build/$PRESET/vv ~/.local/bin/
-
-echo "Installed vv to ~/.local/bin (make sure it's in your PATH)"
+echo
+echo "Installed vv to $INSTALL_DIR (make sure it's in your PATH)"
